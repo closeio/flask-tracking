37d50f83db0b58dc276d92d429c9b83afd1d969f
Phil Freo <phil@philfreo.com>
2013-07-22 13:04:00 -0700

    Tracking#debug() to view request/response

--- flask_tracking/documents.py
+++ flask_tracking/documents.py
@@ -1,4 +1,5 @@
 from mongoengine import *
+import json
 
 class UserAgent(EmbeddedDocument):
     browser = StringField()
@@ -55,3 +56,27 @@ class Tracking(Document):
             query=self.query_params and '?%s' % self.query_params or '',
             status=self.status_code,
             time=self.execution_time)
+
+    def debug(self):
+        ret = '%s %s%s%s\n' % (self.method, self.host, self.path, self.query_params and '?%s' % self.query_params or '')
+        ret += 'REQUEST:\n'
+        ret += self.format_headers(self.request_headers) + '\n'
+        ret += self.format_json(self.request_body.decode()) + '\n'
+        ret += '%s RESPONSE:\n' % self.status_code
+        ret += self.format_headers(self.response_headers) + '\n'
+        ret += self.format_json(self.response_body.decode())
+        return ret
+
+    @staticmethod
+    def format_json(inpt):
+        """Format a string as JSON if possible, otherwise return string"""
+        try:
+            return json.dumps(json.loads(inpt), indent=4)
+        except ValueError:
+            return inpt
+
+    @staticmethod
+    def format_headers(headers):
+        return '\n'.join(['  %s: %s' % (h[0], h[1] if len(h[1]) < 100 else '%s...' % h[1][:100]) for h in headers])
+
+
